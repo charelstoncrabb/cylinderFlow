8b25f180a3dc08f7e243b3e67e4a778cfe440829
charelstoncrabb <ncrabb@ucdavis.edu>
2018-07-22 13:12:45 -0700

    Cleaning up gh-pages branch

--- FEMesh.cpp
+++
@@ -1,9 +0,0 @@
-//
-//  FEMesh.cpp
-//  cylinderFlow
-//
-//  Created by Nicholas Crabb on 7/4/17.
-//  Copyright © 2017 Nicholas Crabb. All rights reserved.
-//
-
-#include "FEMesh.hpp"
--- SolverPoisson.cpp
+++
@@ -1,8 +0,0 @@
-//
-//  SolverPoisson.cpp
-//  cylinderFlow
-//
-//  Created by Charelston Crabb on 7/4/17.
-//  Copyright © 2017 NLC. All rights reserved.
-//
-#include "SolverPoisson.hpp"
